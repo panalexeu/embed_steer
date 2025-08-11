42e214dd2f3aa9e58bef47cf8b669a84b0b2f8f4
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2025-08-11 22:01:23 +0300

    declared EmbedSteerRetrieval

---
+++ src/em_steer_retr.py
@@ -0,0 +1,6 @@
+from mteb.abstasks import AbsTaskRetrieval
+
+
+class EmbedSteerRetrieval(AbsTaskRetrieval):
+    """AbsTaskRetrieval is a base class of NQ and MSMARCO benchmarks in MTEB"""
+    pass
